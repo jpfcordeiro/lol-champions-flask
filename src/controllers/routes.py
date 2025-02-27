688a9206b12a7c4580fde3921ea4f4a4cd7a5ad9
DSM4 <fatec-dsm4@academico.intra>
2025-02-26 22:47:23 -0300

    Ajustes novos

--- src/controllers/routes.py
+++ src/controllers/routes.py
@@ -37,8 +37,21 @@ response = urllib.request.urlopen(url_patch)
 versions = json.loads(response.read())
 latest_version = versions[0] if versions else None
 
+data = {}
+
+if latest_version:
+    url_champions = f'https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/pt_BR/championFull.json'
+    response = urllib.request.urlopen(url_champions)
+    champion_data = json.loads(response.read())['data']
+    
+    data = {}
+    for champ_key, champ_info in champion_data.items():
+        champ_info['image_url'] = f'https://ddragon.leagueoflegends.com/cdn/{latest_version}/img/champion/{champ_info["image"]["full"]}'
+        data[champ_key] = champ_info
+
+
+
 def translate_game_modes(game_modes):
-    """Traduz os modos de jogo recebidos da API para português."""
     return [
         {
             "gameMode": mode["gameMode"],
@@ -49,84 +62,80 @@ def translate_game_modes(game_modes):
 
 
 def init_app(app):
-
+    
+    
     @app.route('/')
     def home():
         return render_template('index.html')
-    
+
 
     @app.route('/rankings', methods=['GET', 'POST'])
     def show_rankings():
         if request.method == 'POST':
             form_data = request.form.to_dict()
-            rankings.update(form_data)
-            return redirect(url_for('show_rankings'))
-        return render_template('rankings.html', rankings=rankings)
-    
+
+            champion = form_data.get('champion')
+            winrate = form_data.get('winrate')
+            pickrate = form_data.get('pickrate')
+            banrate = form_data.get('banrate')
+
+            if champion and winrate and pickrate and banrate:
+                winrate = float(winrate)
+                pickrate = float(pickrate)
+                banrate = float(banrate)
+
+                rankings[champion] = {
+                    'winrate': winrate,
+                    'pickrate': pickrate,
+                    'banrate': banrate
+                }
+
+                return redirect(url_for('show_rankings'))
+
+        return render_template('rankings.html', rankings=rankings, champions=data)
+
 
     @app.route('/builds', methods=['GET', 'POST'])
     def show_builds():
         if request.method == 'POST':
             champion = request.form.get('champion')
-            runes = request.form.get('runes').split(",") 
-            items = request.form.get('items').split(",")
-
-            #Estou usando para remover espaços em branco
-            runes = [rune.strip() for rune in runes]
-            items = [item.strip() for item in items]
-
-            builds.append({"champion": champion, "runes": runes, "items": items})
-            return redirect(url_for('show_builds')) 
-
+            runes = [r.strip()
+                     for r in request.form.get('runes', '').split(',')]
+            items = [i.strip()
+                     for i in request.form.get('items', '').split(',')]
+            builds.append(
+                {"champion": champion, "runes": runes, "items": items})
+            return redirect(url_for('show_builds'))
         return render_template('builds.html', builds=builds)
-    
 
-    @app.route('/champions', methods=['GET', 'POST'])
-    @app.route('/champions/<id>', methods=['GET', 'POST'])
+
+    @app.route('/champions', methods=['GET'])
+    @app.route('/champions/<id>', methods=['GET'])
     def list_champion(id=None):
-        if latest_version:
-            url_champions = f'https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/pt_BR/championFull.json'
-            response = urllib.request.urlopen(url_champions)
-            data = json.loads(response.read())['data']
-        else:
-            data = []
-            
         if id:
-            champ_id = None
-            for champ in data.values():
-                if champ['name'].lower() == id.lower():
-                    champ_id = champ["id"]
-                    break
-
+            champ_id = next((champ["id"] for champ in data.values(
+            ) if champ['name'].lower() == id.lower()), None)
             if champ_id:
                 champinfo_url = f'https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/pt_BR/champion/{champ_id}.json'
                 response = urllib.request.urlopen(champinfo_url)
-                print(champinfo_url)
                 champ_data = json.loads(response.read())['data'][champ_id]
                 return render_template('championsinfo.html', champion=champ_data)
-            else:
-                return 'Campeão não encontrado!', 404
+            return 'Campeão não encontrado!', 404
         return render_template('champions.html', champions=data)
-    
 
-    @app.route('/gamemodes', methods=['GET', 'POST'])
+
+    @app.route('/gamemodes', methods=['GET'])
     def show_gamemodes():
         gamemode_url = 'https://static.developer.riotgames.com/docs/lol/gameModes.json'
         response = urllib.request.urlopen(gamemode_url)
         game_modes_api = json.loads(response.read())
-
         translated_modes = translate_game_modes(game_modes_api)
-
-        print(translated_modes)
-
         return render_template('gamemodes.html', gamemodes=translated_modes)
-    
 
-    @app.route('/itens', methods=['GET', 'POST'])
+
+    @app.route('/itens', methods=['GET'])
     def show_itens():
         items_url = f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/data/pt_BR/item.json"
         response = urllib.request.urlopen(items_url)
         items_data = json.loads(response.read())["data"]
-
-        return render_template('items.html', items=items_data)
-
+        return render_template('items.html', items=items_data, latest_version=latest_version)
